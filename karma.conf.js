75e04f34288556263e738ed980bc853fc4f30326
Maveric Ramos <majaramosp@gmail.com>
2022-03-03 18:31:05 +0000

    Probando TMS

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,8 @@ module.exports = function(config) {
     "use strict";
     config.set({
         frameworks: ["ui5"],
-        browsers: ["Chrome"],
+        browsers: ["ChromeHeadless"],
+        singleRun: true,
         browserConsoleLogOptions: {
             level: "error"
         }
